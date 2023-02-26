1c96ab6aaa62044df4b98392e2a8773fb5dbb8ec
ShadowDancer <ShadowDancer@users.noreply.github.com>
2023-02-26 18:25:07 +0100

    Add project, initial operational version

---
+++ YachtLogHoursCalculator/Calculator/HourCalculator.cs
@@ -0,0 +1,32 @@
+﻿namespace YachtLogHoursCalculator.Calculator
+{
+    public static class HourCalculator
+    {
+        public static Dictionary<string, TimeSpan> Calculate(IEnumerable<LogEntry> entries)
+        {
+            var sorted = entries.OrderBy(n => n.StartTime).ToList();
+
+            var last = new LogEntry(TimeSpan.FromHours(24), "end of day senteniel");
+            sorted.Add(last);
+
+            if (!sorted.Any())
+            {
+                return new Dictionary<string, TimeSpan>();
+            }
+
+            Dictionary<string, TimeSpan> hourSum = new();
+
+            var currentEntry = sorted.First();
+
+            foreach (var nextEntry in sorted.Skip(1))
+            {
+                var elapsed = nextEntry.StartTime - currentEntry.StartTime;
+                hourSum[currentEntry.Type] = hourSum.GetValueOrDefault(currentEntry.Type) + elapsed;
+
+                currentEntry = nextEntry;
+            }
+
+            return hourSum;
+        }
+    }
+}
---
+++ YachtLogHoursCalculator/Calculator/LogEntry.cs
@@ -0,0 +1,7 @@
+﻿namespace YachtLogHoursCalculator.Calculator
+{
+    public record LogEntry(TimeSpan StartTime, string Type)
+    {
+        public static List<string> Types = new List<string>() { "Postój", "Żagle", "Silnik", "Żagle + Silnik" };
+    }
+}
