1746ceacb43b14dfbf62450d3287ee90a6d7ca06
Przemysław Onak <przemekonak@gmail.com>
2025-05-06 01:31:10 +0200

    Internal refactors, add mode to display sum of sail and engine

--- YachtLogHoursCalculator/Calculator/HourCalculator.cs
+++ YachtLogHoursCalculator/Calculator/HourCalculator.cs
@@ -1,32 +1,33 @@
-﻿namespace YachtLogHoursCalculator.Calculator
+﻿namespace YachtLogHoursCalculator.Calculator;
+
+public static class HourCalculator
 {
-    public static class HourCalculator
+    public static Dictionary<EntryType, TimeSpan> Calculate(IEnumerable<LogEntry> entries)
     {
-        public static Dictionary<string, TimeSpan> Calculate(IEnumerable<LogEntry> entries)
+        var sorted = entries.OrderBy(n => n.StartTime).ToList();
+
+        // Entry duration is calculated as the difference between the entry and the next entry
+        // This last entry handles last entry in the day nicely
+        var last = new LogEntry(TimeSpan.FromHours(24), (EntryType.Moored));
+        sorted.Add(last);
+
+        if (!sorted.Any())
         {
-            var sorted = entries.OrderBy(n => n.StartTime).ToList();
-
-            var last = new LogEntry(TimeSpan.FromHours(24), "end of day senteniel");
-            sorted.Add(last);
-
-            if (!sorted.Any())
-            {
-                return new Dictionary<string, TimeSpan>();
-            }
-
-            Dictionary<string, TimeSpan> hourSum = new();
-
-            var currentEntry = sorted.First();
-
-            foreach (var nextEntry in sorted.Skip(1))
-            {
-                var elapsed = nextEntry.StartTime - currentEntry.StartTime;
-                hourSum[currentEntry.Type] = hourSum.GetValueOrDefault(currentEntry.Type) + elapsed;
-
-                currentEntry = nextEntry;
-            }
-
-            return hourSum;
+            return new Dictionary<EntryType, TimeSpan>();
         }
+
+        Dictionary<EntryType, TimeSpan> hourSum = new();
+
+        var currentEntry = sorted.First();
+
+        foreach (var nextEntry in sorted.Skip(1))
+        {
+            var elapsed = nextEntry.StartTime - currentEntry.StartTime;
+            hourSum[currentEntry.Type] = hourSum.GetValueOrDefault(currentEntry.Type) + elapsed;
+
+            currentEntry = nextEntry;
+        }
+            
+        return hourSum;
     }
 }
--- YachtLogHoursCalculator/Calculator/LogEntry.cs
+++ YachtLogHoursCalculator/Calculator/LogEntry.cs
@@ -1,7 +1,48 @@
-﻿namespace YachtLogHoursCalculator.Calculator
+﻿using Microsoft.Extensions.Logging.Abstractions;
+
+namespace YachtLogHoursCalculator.Calculator;
+
+/// <summary>
+/// Represents log event in time, i.e. putting sail up, putting sail down, engine on, engine off.
+/// </summary>
+/// <param name="StartTime">Time of the day when event happened</param>
+/// <param name="Type">Type of events</param>
+public record LogEntry(TimeSpan StartTime, EntryType Type)
 {
-    public record LogEntry(TimeSpan StartTime, string Type)
+}
+
+
+/// <summary>
+/// Represents set of entries to be put in yach log, can represent past aggregation, current day or value to transfer to next day. 
+/// </summary>
+public record LogEntrySet
+{
+    public static LogEntrySet FromEntries(ICollection<LogEntry> entries, LogEntrySet? previousDay = null)
     {
-        public static List<string> Types = new List<string>() { "Postój", "Żagle", "Silnik", "Żagle + Silnik" };
+        var durations = HourCalculator.Calculate(entries);
+        
+        // Add previous day values to current day
+        durations[EntryType.Moored] = durations.GetValueOrDefault(EntryType.Moored) + (previousDay?.Moored ?? TimeSpan.Zero);
+        durations[EntryType.Engine] = durations.GetValueOrDefault(EntryType.Engine) + (previousDay?.Engine ?? TimeSpan.Zero);
+        durations[EntryType.Sail] = durations.GetValueOrDefault(EntryType.Sail) + (previousDay?.Sails ?? TimeSpan.Zero);
+
+        return new LogEntrySet(durations);
     }
+    
+
+    private LogEntrySet(Dictionary<EntryType, TimeSpan> entryDurations)
+    {
+        Moored = entryDurations.GetValueOrDefault(EntryType.Moored);
+        Engine = entryDurations.GetValueOrDefault(EntryType.Engine);
+        Sails = entryDurations.GetValueOrDefault(EntryType.Sail);
+    }
+    
+    public TimeSpan Sails { get; }
+    public TimeSpan Moored { get; }
+    public TimeSpan Engine { get; }
+    
+    public TimeSpan SailsAndEngine => Sails + Engine;
+
+    public TimeSpan Sum => Sails + Engine + Moored;
+    public static LogEntrySet Empty { get; } = new(new Dictionary<EntryType, TimeSpan>());
 }
